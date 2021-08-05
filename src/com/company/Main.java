f8cd5e42ab20aae0079a54f8097214f1a3597822
Vladislav Steere <vssteere@gmail.com>
2021-08-05 17:21:52 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+//method adding together the sum of digits in a number; for example if you run 125, the answer is 8;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("sum of digits in number 125 is " +  sumDigits(125));
+        System.out.println("sum of digits in number -125 is " +  sumDigits(-125));
+        System.out.println("sum of digits in number 5 is " +  sumDigits(5));
+        System.out.println("sum of digits in number 32,123 is " +  sumDigits(32123));
+
+    }
+
+    private static int sumDigits(int number) {
+        if(number<10) {
+            return -1;
+        }
+        int sum = 0;
+        // 125 -> 125 /10 = 12 => 12 * 10 = 120 => 125 - 120 =5
+
+        while (number>0) {
+            // extract the least significant diigt
+            int digit = number % 10;
+            sum += digit;
+
+            //drop the least significant digit
+            number /= 10; // number = number /10;
+
+
+        }
+        return sum;
+    }
+}
